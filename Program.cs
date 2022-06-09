613134baca6267ac81bb61deb6f0575a2e12f1f2
Robert <rob5300@hotmail.com>
2022-06-09 22:16:10 +0100

    Improve error message and error handling for some possible file exceptions.

--- Program.cs
+++ Program.cs
@@ -34,9 +34,11 @@ Dictionary<string, string> translationData= new Dictionary<string, string>();
 List<KVObject> decals = new List<KVObject>();
 Dictionary<string, KV3Decal> newDecals = new Dictionary<string, KV3Decal>();
 
+string decalsSubrectPath = Path.Combine(targetFolder, "decals_subrect.txt");
 try
 {
-    using (var stream = File.Open(Path.Combine(targetFolder, "decals_subrect.txt"), FileMode.Open))
+    Console.WriteLine($"-= Will load '{decalsSubrectPath}' for translation data and decals...");
+    using (var stream = File.Open(decalsSubrectPath, FileMode.Open))
     {
         decals_subrect = serialiser.Deserialize(stream);
         //Sort out the translation data from the other decals
@@ -53,12 +55,20 @@ try
         }
     }
 }
-catch (Exception)
+catch (FileNotFoundException e)
 {
-    Console.WriteLine("ERROR: Unable to find 'decals_subrect.txt'.");
-    return;
+    ExitFromError(e, decalsSubrectPath);
 }
-    
+catch(UnauthorizedAccessException e)
+{
+    Console.WriteLine("!!! Program did not have permission to access this file. Move it elsewhere or run this as admin !!!");
+    ExitFromError(e, decalsSubrectPath);
+}
+catch (Exception e)
+{
+    ExitFromError(e);
+}
+
 string outputFolder;
 
 if (Directory.Exists(targetFolder))
@@ -71,22 +81,22 @@ if (Directory.Exists(targetFolder))
         Directory.CreateDirectory(outputFolder);
     }
 
-    Log.WriteLine($"-= Will process decals...");
+    Console.WriteLine("-= Will process decals...");
 
     string decalOutPath = Path.Combine(targetFolder, "data", "decal");
     if (!Directory.Exists(decalOutPath))
     {
         Directory.CreateDirectory(decalOutPath);
     }
-    
+
     var createdDecals = CreateDecalFiles(decals, decalOutPath);
     newDecals = createdDecals.ToDictionary(x => x.Name);
 
-    Log.WriteLine($"-= Will process surfaces files...");
+    Console.WriteLine("-= Will process surfaces files...");
 
     //Look for surfaceproperties files and process them
     string surfacePropertiesFilePath = null;
-    foreach(var potentialSurfaceProperties in Directory.EnumerateFiles(targetFolder))
+    foreach (var potentialSurfaceProperties in Directory.EnumerateFiles(targetFolder))
     {
         string name = Path.GetFileNameWithoutExtension(potentialSurfaceProperties).ToLower();
         if (name.Contains("surfaceproperties") && !name.Contains("manifest"))
@@ -96,66 +106,98 @@ if (Directory.Exists(targetFolder))
             surfacePropertiesFilePath = potentialSurfaceProperties;
         }
     }
-    
-    //Write log to program dir
-    //Log.WriteToFile(Directory.GetCurrentDirectory());
 
     stopwatch.Stop();
     Console.WriteLine($"Done! Took {stopwatch.Elapsed}.");
 }
 else
 {
-    Log.WriteLine("-= Directory doesnt exist, exiting...");
+    Console.WriteLine("-= Directory doesnt exist, exiting...");
 }
 Console.ReadKey();
 
 void ProcessSurfacesFile(string filePath, string outputFolder)
 {
-    using (var stream = File.Open(filePath, FileMode.Open))
+    try
     {
-        KVObject? kvObject = serialiser.Deserialize(stream);
-        //Make a list of all the surfaces
-        List<KVObject> surfaceObjects = new List<KVObject>();
-        surfaceObjects.Add(kvObject);
-        surfaceObjects.AddRange(kvObject.Children.Where(x => x.Children.Count() > 0));
-        
-        foreach(var surfaceObject in surfaceObjects)
+        using (var stream = File.Open(filePath, FileMode.Open))
         {
-            string surfaceName = KV3Surface.Prefix + surfaceObject.Name;
-            string surfaceFile = Path.Combine(outputFolder, $"{surfaceName.ToLower()}.surface");
-            KV3Surface newSurface = new KV3Surface(surfaceObject);
-            newSurface.Name = KV3Surface.Prefix + newSurface.Name;
-            KVValue? gamematerial = newSurface.gamematerial;
-            if (gamematerial != null)
+            KVObject? kvObject = serialiser.Deserialize(stream);
+            //Make a list of all the surfaces
+            List<KVObject> surfaceObjects = new List<KVObject>();
+            surfaceObjects.Add(kvObject);
+            surfaceObjects.AddRange(kvObject.Children.Where(x => x.Children.Count() > 0));
+
+            foreach (var surfaceObject in surfaceObjects)
             {
-                if (translationData.TryGetValue((string)gamematerial, out string decalName))
+                string surfaceName = KV3Surface.Prefix + surfaceObject.Name;
+                string surfaceFile = Path.Combine(outputFolder, $"{surfaceName.ToLower()}.surface");
+                KV3Surface newSurface = new KV3Surface(surfaceObject);
+                newSurface.Name = KV3Surface.Prefix + newSurface.Name;
+                KVValue? gamematerial = newSurface.gamematerial;
+                if (gamematerial != null)
                 {
-                    newDecals.TryGetValue(decalName, out KV3Decal? decal);
-                    newSurface.gamematerial_decal = decal;
+                    if (translationData.TryGetValue((string)gamematerial, out string decalName))
+                    {
+                        newDecals.TryGetValue(decalName, out KV3Decal? decal);
+                        newSurface.gamematerial_decal = decal;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"WARNING: Unable to find decal '{gamematerial}' translation for surface '{surfaceName}'.");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine($"WARNING: Unable to find decal '{gamematerial}' translation for surface '{surfaceName}'.");
-                }
+
+                File.WriteAllText(surfaceFile, newSurface.ToString());
+                Console.WriteLine($"Made surface file for '{surfaceName}'.");
             }
-           
-            File.WriteAllText(surfaceFile, newSurface.ToString());
-            Console.WriteLine($"Made surface file for '{surfaceName}'.");
         }
+    }
+    catch(Exception e)
+    {
+        ExitFromError(e, filePath);
     }
 }
 
 List<KV3Decal> CreateDecalFiles(IEnumerable<KVObject> decals, string outputFolder)
 {
-    List<KV3Decal> newDecals = new List<KV3Decal>();
-    foreach (var decal in decals)
+    try
     {
-        string decalName = decal.Name;
-        string decalFile = Path.Combine(outputFolder, $"{decalName.ToLower()}.decal");
-        KV3Decal newDecal = new KV3Decal(decal);
-        File.WriteAllText(decalFile, newDecal.ToString());
-        Console.WriteLine($"Made decal file for '{newDecal.Name}'.");
-        newDecals.Add(newDecal);
+        List<KV3Decal> newDecals = new List<KV3Decal>();
+        foreach (var decal in decals)
+        {
+            string decalName = decal.Name;
+            string decalFile = Path.Combine(outputFolder, $"{decalName.ToLower()}.decal");
+            KV3Decal newDecal = new KV3Decal(decal);
+            File.WriteAllText(decalFile, newDecal.ToString());
+            Console.WriteLine($"Made decal file for '{newDecal.Name}'.");
+            newDecals.Add(newDecal);
+        }
+        return newDecals;
     }
-    return newDecals;
+    catch(Exception e)
+    {
+        ExitFromError(e);
+        return null;
+    }
+}
+
+void ExitFromError(Exception e, string attemptedPath = null)
+{
+    if(!string.IsNullOrEmpty(attemptedPath))
+    {
+        Console.WriteLine($"Unable to open '{Path.GetFileName(attemptedPath)}'. Program will now exit.");
+    }
+    else
+    {
+        Console.WriteLine("Critical error occurred. Program will now exit.");
+    }
+    
+    if(!string.IsNullOrEmpty(attemptedPath))
+    {
+        Console.WriteLine($"Attempted path: '{decalsSubrectPath}'");
+    }
+    Console.WriteLine($"ERROR: {e.GetType().Name}, {e.Message}.");
+    Console.WriteLine("Error Stack:\n" + e.StackTrace);
+    Environment.Exit(1);
 }
